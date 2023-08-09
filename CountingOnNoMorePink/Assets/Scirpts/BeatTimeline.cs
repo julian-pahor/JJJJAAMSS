d4e7b59c83b998102d3cdf955c5317442ae508d8
julian-pahor <julian.pahor1998@gmail.com>
2023-08-09 10:42:55 +0930

    Will unity break from the fmod package?

--- CountingOnNoMorePink/Assets/Scirpts/BeatTimeline.cs
+++ CountingOnNoMorePink/Assets/Scirpts/BeatTimeline.cs
@@ -10,28 +10,47 @@ public class BeatTimeline : MonoBehaviour
     float timer;
     int index;
 
-    private void Update()
-    {
-        if (attackEvents.Count == 0)
-            return;
+    //private void Update()
+    //{
+    //    if (attackEvents.Count == 0)
+    //        return;
 
-        timer += Time.deltaTime;
+    //    timer += Time.deltaTime;
 
-        if(timer >= beatLength)
-        {
+    //    if(timer >= beatLength)
+    //    {
            
-            timer = 0;
-            attackEvents[index].Fire();
-            index++;
+    //        timer = 0;
+    //        attackEvents[index].Fire();
+    //        index++;
 
-            if(index >= attackEvents.Count)
-            {
-                index = 0;
-            }
+    //        if(index >= attackEvents.Count)
+    //        {
+    //            index = 0;
+    //        }
 
-        }
+    //    }
+    //}
+
+
+    private void Awake()
+    {
+        BeatBroadcast.instance.timelineInfo.onBeatTrigger += Beat;
     }
 
+    private void OnDestroy()
+    {
+        BeatBroadcast.instance.timelineInfo.onBeatTrigger -= Beat;
+    }
 
+    private void Beat(int bar, int beat)
+    {
+        attackEvents[index].Fire();
+        index++;
 
+        if(index >= attackEvents.Count)
+        {
+            index = 0;
+        }
+    }
 }
