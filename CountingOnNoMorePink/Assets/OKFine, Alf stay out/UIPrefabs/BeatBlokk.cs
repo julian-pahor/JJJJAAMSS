f160750f705c01978ca1417f2ee056ae79654bb8
julian-pahor <julian.pahor1998@gmail.com>
2023-08-15 11:47:42 +0930

    Timeline UI is almost fully functioning

---
+++ CountingOnNoMorePink/Assets/OKFine, Alf stay out/UIPrefabs/BeatBlokk.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatBlokk : MonoBehaviour
+{
+    public List<BeatSlot> slots = new List<BeatSlot>();
+    // Start is called before the first frame update
+}
