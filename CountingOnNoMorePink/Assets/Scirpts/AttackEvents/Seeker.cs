64fe22131db23e3127f32bdc67ac920f66807105
Muglamp <s214705@students.aie.edu.au>
2023-08-21 16:43:39 +0930

    Beattimer badness

--- CountingOnNoMorePink/Assets/Scirpts/AttackEvents/Seeker.cs
+++ CountingOnNoMorePink/Assets/Scirpts/AttackEvents/Seeker.cs
@@ -6,19 +6,19 @@ using UnityEngine;
 public class Seeker : AttackEvent
 {
 
-    public float safeTime;
-    public float height;
+    //public float safeTime;
+    //public float height;
  
 
     public override void Fire()
     {
         
 
-        Vector3 pos = Wobbit.instance.player.position + (Vector3.up * height);
+        //Vector3 pos = Wobbit.instance.player.position + (Vector3.up * height);
 
         //GameObject go = Instantiate(Wobbit.instance.warning, pos, Quaternion.Euler(new Vector3(-90f,0f,0)));
-        BoomBlock b = (Instantiate(Wobbit.instance.zoneFab, Wobbit.instance.player.position, Quaternion.identity));
-        b.Initialise(safeTime,pos, Wobbit.instance.player.position);
+        DelayedDangerZone b = (Instantiate(Wobbit.instance.delayedDangerZoneTest, Wobbit.instance.player.position, Quaternion.identity));
+        b.InitialiseOnBeat(1,1);
        // go.transform.SetParent(b.transform);
     }
 
