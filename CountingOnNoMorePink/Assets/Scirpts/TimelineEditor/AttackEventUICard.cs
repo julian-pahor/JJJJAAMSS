e15c86bbc3831bc0d2621ec62333a977d4ec3343
Muglamp <s214705@students.aie.edu.au>
2023-09-25 16:25:24 +0930

    parry bug don forgot

--- CountingOnNoMorePink/Assets/Scirpts/TimelineEditor/AttackEventUICard.cs
+++ CountingOnNoMorePink/Assets/Scirpts/TimelineEditor/AttackEventUICard.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using DG.Tweening;
-using UnityEditor.Build;
 using UnityEngine.EventSystems;
 
 public class AttackEventUICard : MonoBehaviour
