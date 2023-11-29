00f411444339084f30fea2851f7027e2bcdc31a2
Muglamp <99298207+Muglamp@users.noreply.github.com>
2023-11-29 11:38:42 +1030

    super cool new beam

--- CountingOnNoMorePink/Assets/Scirpts/AttackEvents/Effects/GreenBeam.cs
+++ CountingOnNoMorePink/Assets/Scirpts/AttackEvents/Effects/GreenBeam.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
+
 
 public class GreenBeam : MonoBehaviour
 {
@@ -12,8 +14,16 @@ public class GreenBeam : MonoBehaviour
     public Transform player;
     public Transform target;
 
+    public VisualEffect pillarBoom;
+    public VisualEffect parryBoom;
+    public Transform anchor;
+
     float timer;
 
+    private void Start()
+    {
+        pillarBoom.Stop();
+    }
     private void Update()
     {
         if (timer <= 0)
@@ -30,10 +40,15 @@ public class GreenBeam : MonoBehaviour
             beam.enabled = false;
     }
 
-    [ContextMenu("TEST BEEM")]
     public void BeamOn()
     {
         timer = beamTime;
         beam.enabled = true;
+        
+        anchor.LookAt(transform.position);
+ 
+        parryBoom.Play();
+        pillarBoom.Play();
     }
+
 }
